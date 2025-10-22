11597a8b9b177055a2f4becc070c0c12dede49ba
Joao Paulo <joaopaulodesousapoubel@gmail.com>
2025-10-22 18:29:30 -0300

    Delete sistema_biblioteca_testeSplashScreen directory

--- sistema_biblioteca_testeSplashScreen/sistema_biblioteca-mindas-db/src/sql/relatorio_leitor.sql
+++
@@ -1,7 +0,0 @@
-select le.id_leitor,
-        le.CPF,
-        le.nome,
-        le.email,
-        le.telefone
-    from leitor le
-    ORDER BY le.nome
--- sistema_biblioteca_testeSplashScreen/sistema_biblioteca-mindas-db/src/sql/relatorio_livro.sql
+++
@@ -1,7 +0,0 @@
-select li.id_livro,
-        li.titulo,
-        li.autor,
-        li.ano_publicacao,
-        li.editora
-    from livro li
-    ORDER BY li.titulo
