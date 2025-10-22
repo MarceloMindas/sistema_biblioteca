6a79778607f9a76dc7be9ae27fbe723833977a72
Joao Paulo <joaopaulodesousapoubel@gmail.com>
2025-10-22 18:29:36 -0300

    Delete sistema_biblioteca_SplashScreenTeste directory

--- sistema_biblioteca_SplashScreenTeste/sistema_biblioteca-mindas-db/sql/relatorio_1.sql
+++
@@ -1,9 +0,0 @@
-SELECT em.id_emprestimo,
-       le.nome AS nome_leitor,
-       li.titulo AS titulo_livro,
-       em.data_emprestimo,
-       em.data_devolucao_realizada
-FROM emprestimo em
-JOIN leitor le ON em.id_leitor = le.id_leitor
-JOIN livro li ON em.id_livro = li.id_livro
-ORDER BY em.data_emprestimo;
--- sistema_biblioteca_SplashScreenTeste/sistema_biblioteca-mindas-db/sql/relatorio_2.sql
+++
@@ -1,6 +0,0 @@
-SELECT li.titulo AS livro,
-       COUNT(em.id_emprestimo) AS total_emprestimos
-FROM emprestimo em
-JOIN livro li ON em.id_livro = li.id_livro
-GROUP BY li.titulo
-ORDER BY total_emprestimos DESC;
